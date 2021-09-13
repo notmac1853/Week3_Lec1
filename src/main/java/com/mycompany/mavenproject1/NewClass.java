db855106e4f6f5ba5323f36ffa645f38be375017
Marco Chavez <>
2021-09-13 09:50:43 -0400

    Initial Commit - project created

---
+++ src/main/java/com/mycompany/mavenproject1/NewClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author soblab
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        int x=0;
+        
+        do{
+            x++;
+            System.out.println("i = " +x);
+        }while(x!=10);
+    }
+    
+}
